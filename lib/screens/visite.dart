cb28aa36c60b3d8485e5d14d0fca98bf25ae468b
mattoussi <aymenmattoussi050@gmail.com>
2023-03-09 17:05:41 +0100

    update

--- lib/screens/visite.dart
+++ lib/screens/visite.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: library_private_types_in_public_api, camel_case_types, prefer_const_constructors, avoid_print, unnecessary_new, non_constant_identifier_names, avoid_unnecessary_containers
 
-import 'dart:ffi';
+
 
 import 'package:firebase_auth_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/retry.dart';
+
 
 class visite extends StatefulWidget {
 const visite({Key? key}) : super(key: key);
