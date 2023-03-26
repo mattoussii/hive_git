95c6bc5a3c46e7084e42bea784b9cff3e96f7646
mattoussi <aymenmattoussi050@gmail.com>
2023-03-26 15:46:11 +0100

    formfield

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,9 @@
 
 
 
-// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_new
 
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,21 +21,33 @@ class _loginScreenState extends State<loginScreen> {
   final _emailController =TextEditingController();
   final _passwordController =TextEditingController();
 
+
+
+
   Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword
+
+     var formdata = formstate.currentState ;
+  if(formdata!.validate()){
+        await FirebaseAuth.instance.signInWithEmailAndPassword
     ( email: _emailController.text.trim(), 
     password: _passwordController.text.trim()) ;
+  }
+      else{
+             AwesomeDialog(
+            context: context,
+            dialogType: DialogType.warning,
+            animType: AnimType.rightSlide,
+            body:  Text('please add a valid account'),
+            btnOkOnPress: () {},
+            ).show();
+      }
+
   }
 
 
   void openSignupScreen(){
     Navigator.of(context).pushReplacementNamed('signupScreen');
   }
-
-
-
-
-
 
 
 
@@ -47,6 +60,8 @@ class _loginScreenState extends State<loginScreen> {
     _emailController.dispose();
     _passwordController.dispose();
   }
+  GlobalKey<FormState>  formstate  = new  GlobalKey<FormState>() ;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -68,131 +83,151 @@ class _loginScreenState extends State<loginScreen> {
        SafeArea(   
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('images/hive.png',
-                height: 150,),
-                SizedBox(height :10),
-                Text(
-                  'SIGN IN',
-                  style: GoogleFonts.robotoCondensed(
-                    fontSize: 40,fontWeight: FontWeight.bold, color: Colors.orange,
-                  ),
-                  ),
-                                 
+            child: Form(
+               
+              key: formstate ,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('images/hive.png',
+                  height: 150,),
+                  SizedBox(height :10),
                   Text(
-                    'welcome back nice to see you again',
-                     style: GoogleFonts.robotoCondensed(
-                    fontSize: 18,fontWeight: FontWeight.bold, color: Colors.amber[800],
-                  ),
-                  ),
-                        
-                  SizedBox(height: 50,),         
-          
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.person ,color: Colors.black,),
-                           border: InputBorder.none,
-                           hintText: 'Email',
-                           ),
-                      ),
+                    'SIGN IN',
+                    style: GoogleFonts.robotoCondensed(
+                      fontSize: 40,fontWeight: FontWeight.bold, color: Colors.orange,
                     ),
-                  ),
-                     
-                  SizedBox(height: 10,),
-          
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextField(                       
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.password_rounded ,color: Colors.black,),
-                           border: InputBorder.none,
-                           hintText: 'password',
-                           ),
-                      ),
                     ),
-                  ),
-                                       
-                  SizedBox(height: 10,),
-                   
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 85),
-                    child: GestureDetector(
-                      onTap: signIn ,
+                                   
+                    Text(
+                      'welcome back nice to see you again',
+                       style: GoogleFonts.robotoCondensed(
+                      fontSize: 18,fontWeight: FontWeight.bold, color: Colors.amber[800],
+                    ),
+                    ),
+                          
+                    SizedBox(height: 50,),         
+                      
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
                       child: Container(
-                        padding: EdgeInsets.all(16),
-                        decoration:  BoxDecoration(
-                          color: Colors.green[700],
+                        decoration:BoxDecoration(
+                          color: Colors.white,
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        child: Center(child: Text(
-                          'Sign in',
-                          style: GoogleFonts.robotoCondensed(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                          
-                          ),
-                          ),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction ,
+                          validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your email address';
+                              } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },                         
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.person ,color: Colors.black,),
+                             border: InputBorder.none,
+                             hintText: 'Email',
+                             ),
+                        ),
                       ),
                     ),
-                  ),
-
-                  SizedBox(height: 10),
-          
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Not yet a menber ? ',
-                         style: GoogleFonts.robotoCondensed(
+                       
+                    SizedBox(height: 20,),                     
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Container(
+                        decoration:BoxDecoration(
                           color: Colors.white,
-                          fontWeight: FontWeight.bold,)
-                        
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: TextFormField(  
+                          autovalidateMode: AutovalidateMode.onUserInteraction ,
+                           validator:(text){
+                            if(text!.length < 6){
+                              return "password min caractere 6" ;
+                            }
+                            return null ;
+                          },                     
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.password_rounded ,color: Colors.black,),
+                             border: InputBorder.none,
+                             hintText: 'password',
+                             ),
+                        ),
                       ),
-          
-                      GestureDetector(
-                        onTap:  openSignupScreen,
-                        child: Text(
-                          'Sign up Now',
+                    ),
+                                         
+                    SizedBox(height: 10,),
+                     
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 85),
+                      child: GestureDetector(
+                        onTap:  signIn ,
+                        
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          decoration:  BoxDecoration(
+                            color: Colors.green[700],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Center(child: Text(
+                            'Sign in',
                             style: GoogleFonts.robotoCondensed(
-                            color: Colors.amber[900],
-                            fontWeight: FontWeight.bold,
-                            ),),
-                      )
-                    ],
-                  )
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-              ],
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            
+                            ),
+                            ),
+                        ),
+                      ),
+                    ),
+            
+                    SizedBox(height: 10),
+                      
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Not yet a menber ? ',
+                           style: GoogleFonts.robotoCondensed(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,)
+                          
+                        ),
+                      
+                        GestureDetector(
+                          onTap:  openSignupScreen,
+                          child: Text(
+                            'Sign up Now',
+                              style: GoogleFonts.robotoCondensed(
+                              color: Colors.amber[900],
+                              fontWeight: FontWeight.bold,
+                              ),),
+                        )
+                      ],
+                    )
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, unused_field, avoid_single_cascade_in_expression_statements, unrelated_type_equality_checks
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, unused_field, avoid_single_cascade_in_expression_statements, unrelated_type_equality_checks, unnecessary_new
 
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,13 +19,25 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final _confirmpasswordController =TextEditingController();
 
   Future signUp() async {
-   if(passwordConfirmed()){
-     await FirebaseAuth.instance.createUserWithEmailAndPassword(
-       email: _emailController.text.trim(), 
-       password: _passwordController.text.trim()
-       );
-       Navigator.of(context).pushNamed('/');
-   }
+  var formdata = formstate.currentState ;
+    if(formdata!.validate()){
+      if(passwordConfirmed()){
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(), 
+          password: _passwordController.text.trim()
+          );
+          Navigator.of(context).pushNamed('/');
+      }
+    }
+    else{
+             AwesomeDialog(
+            context: context,
+            dialogType: DialogType.warning,
+            animType: AnimType.rightSlide,
+            body:  Text('add a valid account'),
+            btnOkOnPress: () {},
+            ).show();
+      }
   }
 
   bool passwordConfirmed(){
@@ -38,11 +51,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
 
   void openSignupScreen(){
-    Navigator.of(context).pushReplacementNamed('loginScreen');
-        
-
-     
+    Navigator.of(context).pushReplacementNamed('loginScreen'); 
   }
+
+
+    GlobalKey<FormState>  formstate  = new  GlobalKey<FormState>() ;
+
 
   @override
 
@@ -75,165 +89,192 @@ class _SignUpScreenState extends State<SignUpScreen> {
        SafeArea(   
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('images/hive.png',
-                height: 150,),
-                SizedBox(height :10),
-                Text(
-                  'SIGN UP',
-                  style: GoogleFonts.robotoCondensed(
-                    fontSize: 40,fontWeight: FontWeight.bold, color: Colors.orange,
-                  ),
-                  ),
-                           
+            child: Form( 
+             
+              key: formstate ,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('images/hive.png',
+                  height: 150,),
+                  SizedBox(height :10),
                   Text(
-                    'welcome ! Here you can sign up ',
-                     style: GoogleFonts.robotoCondensed(
-                    fontSize: 18,fontWeight: FontWeight.bold, color: Colors.amber[800],
-                  ),
-                  ),
-                       
-                  SizedBox(height: 50,),
-
-
-                  
-                  SizedBox(height: 10,),
-
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.email_rounded ,color: Colors.black,),
-                           border: InputBorder.none,
-                           hintText: 'Email',
-                           ),
-                      ),
+                    'SIGN UP',
+                    style: GoogleFonts.robotoCondensed(
+                      fontSize: 40,fontWeight: FontWeight.bold, color: Colors.orange,
                     ),
-                  ),
-                  
-                  SizedBox(height: 10,),
-                   
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.password_rounded ,color: Colors.black,),
-                           border: InputBorder.none,
-                           hintText: 'password',
-                           ),
-                      ),
                     ),
-                  ),
-
-                  SizedBox(height: 10,),
-
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextField(
-                        
-                        controller: _confirmpasswordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.password ,color: Colors.black,),
-                           border: InputBorder.none,
-                           hintText: 'confirm password',
-                           ),
-                      ),
+                             
+                    Text(
+                      'welcome ! Here you can sign up ',
+                       style: GoogleFonts.robotoCondensed(
+                      fontSize: 18,fontWeight: FontWeight.bold, color: Colors.amber[800],
                     ),
-                  ),
-
-                  SizedBox(height: 10,),
-
-
-
-                  SizedBox(height: 10,),
-                  
-
-
-                  SizedBox(height: 10,),
-          
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 85),
-                    child: GestureDetector(
-                      onTap: signUp ,
+                    ),
+                         
+                    SizedBox(height: 50,),
+            
+            
+                    
+                    SizedBox(height: 10,),
+            
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
                       child: Container(
-                        padding: EdgeInsets.all(16),
-                        decoration:  BoxDecoration(
-                          color: Colors.green[700],
+                        decoration:BoxDecoration(
+                          color: Colors.white,
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        child: Center(child: Text(
-                          'Sign up',
-                          style: GoogleFonts.robotoCondensed(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                          
-                          ),
-                          ),
+                        child: TextFormField(
+                           autovalidateMode: AutovalidateMode.onUserInteraction ,
+                                  validator: (value) {
+                                    if (value!.isEmpty) {
+                                      return 'Please enter your email address';
+                                    } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                      return 'Please enter a valid email address';
+                                    }
+                                    return null;
+                                  },
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email_rounded ,color: Colors.black,),
+                             border: InputBorder.none,
+                             hintText: 'Email',
+                             ),
+                        ),
                       ),
                     ),
-                  ),
-                
-                  SizedBox(height: 10),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Already a menber  ',
-                         style: GoogleFonts.robotoCondensed(
+                    
+                    SizedBox(height: 10,),
+                     
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Container(
+                        decoration:BoxDecoration(
                           color: Colors.white,
-                          fontWeight: FontWeight.bold,)
-                        
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: TextFormField(
+                           autovalidateMode: AutovalidateMode.onUserInteraction ,
+                           validator:(text){
+                            if(text!.length < 6){
+                              return "password min caractere 6" ;
+                            }
+                            return null ;
+                          }, 
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.password_rounded ,color: Colors.black,),
+                             border: InputBorder.none,
+                             hintText: 'password',
+                             ),
+                        ),
                       ),
-          
-                      GestureDetector(
-                        onTap:  openSignupScreen,
-                        child: Text(
-                          'Sign in Here',
+                    ),
+            
+                    SizedBox(height: 10,),
+            
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Container(
+                        decoration:BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: TextFormField(
+                           autovalidateMode: AutovalidateMode.onUserInteraction ,
+                           validator:(text){
+                            if(text!.length < 6){
+                              return "password min caractere 6" ;
+                            }
+                            return null ;
+                          }, 
+                          
+                          controller: _confirmpasswordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.password ,color: Colors.black,),
+                             border: InputBorder.none,
+                             hintText: 'confirm password',
+                             ),
+                        ),
+                      ),
+                    ),
+            
+                    SizedBox(height: 10,),
+            
+            
+            
+                    SizedBox(height: 10,),
+                    
+            
+            
+                    SizedBox(height: 10,),
+                      
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 85),
+                      child: GestureDetector(
+                        onTap: signUp ,
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          decoration:  BoxDecoration(
+                            color: Colors.green[700],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Center(child: Text(
+                            'Sign up',
                             style: GoogleFonts.robotoCondensed(
-                            color: Colors.amber[900],
-                            fontWeight: FontWeight.bold,
-                            ),),
-                      )
-                    ],
-                  )
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-              ],
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            
+                            ),
+                            ),
+                        ),
+                      ),
+                    ),
+                  
+                    SizedBox(height: 10),
+            
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already a menber  ',
+                           style: GoogleFonts.robotoCondensed(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,)
+                          
+                        ),
+                      
+                        GestureDetector(
+                          onTap:  openSignupScreen,
+                          child: Text(
+                            'Sign in Here',
+                              style: GoogleFonts.robotoCondensed(
+                              color: Colors.amber[900],
+                              fontWeight: FontWeight.bold,
+                              ),),
+                        )
+                      ],
+                    )
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                ],
+              ),
             ),
           ),
         ),
